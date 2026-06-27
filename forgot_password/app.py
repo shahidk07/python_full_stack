56cbea55723fea3e0eeb4f4a95a4fbc76837fef4
shahidkx07 <shahidkhan88892@gmail.com>
2026-06-27 17:26:16 +0530

    working on forgot_password module

--- forgot_password/app.py
+++ forgot_password/app.py
@@ -1,6 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask, request, redirect, render_template, session
+from random import randint
+import smtplib
+from email.message import EmailMessage
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/send_otp'):
-    
+otp = randint(100000, 999999)
+
+
+@app.route("/send_otp", methods=["POST"])
+def send_otp():
+    email = request.form["email"]
+    otp = str(randint(100000, 999999))
+
+    session["email"] = email
+    session["otp"] = otp
+
+    # create email
+    msg = EmailMessage()
+    msg["Subject"] = "Password Reset OTP Testing"
+    msg["From"] = "viperoflegendkiller@gmail.com"
+    msg["To"] = email
+
+    msg.set_content(f"your otp is {otp}")
+
+    # send email
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
