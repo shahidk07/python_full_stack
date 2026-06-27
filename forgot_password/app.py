be05e3db935f0df6c7a625acb3f5c89dd5387f1e
shahidkx07 <shahidkhan88892@gmail.com>
2026-06-27 22:01:36 +0530

    created email checking and password reset endpoints

--- forgot_password/app.py
+++ forgot_password/app.py
@@ -1,18 +1,33 @@
 from flask import Flask, request, redirect, render_template, session
 from random import randint
+import sqlite3
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+app.secret_key = "some-random-secret-key"
 
-otp = randint(100000, 999999)
+@app.route("/forgot_password",methods=["GET"])
+def send_page():
+    return render_template('forgot.html')
 
 
 @app.route("/send_otp", methods=["POST"])
 def send_otp():
-    email = request.form["email"]
-    otp = str(randint(100000, 999999))
+    
+    email = request.form.get("email")
+    if not email:
+        return "<script>alert('Email is required');window.location='/forgot_password';</script>"
 
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+
+    row = conn.execute("SELECT * FROM forgot_password_app WHERE email=?", (email,)).fetchone()
+    conn.close()
+    if not row:
+        return "<script>alert('We do not have an account registered with this email id')</script>"
+        
+    otp = str(randint(100000, 999999))
     session["email"] = email
     session["otp"] = otp
 
@@ -25,4 +40,44 @@ def send_otp():
     msg.set_content(f"your otp is {otp}")
 
     # send email
+    #Secure Socket Layer
+    #SSL is a tech that encrypts data sent between computers over a network
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login("viperoflegendkiller@gmail.com","qmkr qfhl uepd ktra")
+        smtp.send_message(msg)
+        
+    return redirect("/verify")
+
+@app.route("/verify",methods=["POST","GET"])
+
+def verify():
+    if(request.method=="GET"):
+        return render_template("verify.html")
+    
+    else:
+        entered_otp=request.form["otp"]
+        sent_otp=session["otp"]
+        if sent_otp==entered_otp:
+            return render_template("reset.html")
+        else:
+            return "<script>alert('Invalid OTP');window.location='/verify';</script>"
+@app.route("/reset",methods=["POST","GET"])
+
+def reset():
+    if(request.method=="GET"):
+        return render_template("reset.html")
+    else:
+        email=session["email"]
+        password=request.form["password"]
+        
+        conn = sqlite3.connect('database.db')
+        print("Database Connection Established")
+        conn.row_factory = sqlite3.Row
+        conn.execute("UPDATE forgot_password_app set password=? WHERE email=?", (password, email))
+        conn.commit()
+        conn.close()
+        return "<script>alert('Password changed successfully');</script>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
